f410d7c1d987f8611bc37d1cc50b8cccab857bc1
boucara <traorebelco@gmail.com>
2018-03-20 11:11:08 +0100

    interface de connexion

--- app/src/main/java/com/mbds/nfcapplicationsmartphone/MainActivity.java
+++ app/src/main/java/com/mbds/nfcapplicationsmartphone/MainActivity.java
@@ -1,13 +1,30 @@
 package com.mbds.nfcapplicationsmartphone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnLog ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnLog= (Button) findViewById(R.id.boutonLog);
+
+        btnLog.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ConnectionActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+
     }
 }
